0eef2bd4d680b1162b233321d92f063ee757aef1
shikaiJack <shi863799621@163.com>
2018-10-06 23:47:48 +0800

    branch commit

--- microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
@@ -24,7 +24,7 @@ public class Dept implements Serializable// entity --orm--- db_table
 	{
 		super();
 		this.dname = dname;
-		System.out.println("===================");
+		System.out.println("==========");
 	}
 	
 	
